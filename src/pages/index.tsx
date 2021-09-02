4ebfa9ef938f8befdc1fcda98ce4a2e4bee153b8
whitevacance <whitevacance@gmail.com>
2021-09-02 17:38:58 +0900

    기본 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import theme from 'contexts/theme';
-import Button from 'components/Button/DefaultBtn';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const Home = (): JSX.Element => {
+function Home(): JSX.Element {
   return (
-    <div>
+    <div css={{ padding: '4em' }}>
       <h1 css={{ width: '100%', color: theme.palette.primary.main }}>
         TODO LIST
       </h1>
-      <div>asfasdf</div>
+      <div css={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+        <TextField label="할일을 입력하세요" size="small" variant="outlined" />
+        <Button variant="contained" size="large" color="primary">
+          등록
+        </Button>
+      </div>
       <ul></ul>
     </div>
   );
-};
+}
 
 export default Home;
