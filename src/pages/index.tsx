7023dd1c57e6fc82b8ab20ebfb9385bd318ffc72
whitevacance <whitevacance@gmail.com>
2021-09-02 18:10:34 +0900

    ㅇ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,59 @@
+import { useState } from 'react';
 import theme from 'contexts/theme';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-function Home(): JSX.Element {
+const DB = [
+  {
+    id: 1,
+    text: 'Hello',
+    done: false,
+  },
+  {
+    id: 2,
+    text: '2222222',
+    done: false,
+  },
+];
+
+function Todo(): JSX.Element {
+  const [text, setText] = useState();
+
+  const handleChangeText = (e) => setText(e.target.value);
+
   return (
     <div css={{ padding: '4em' }}>
       <h1 css={{ width: '100%', color: theme.palette.primary.main }}>
         TODO LIST
       </h1>
       <div css={{ display: 'flex', alignItems: 'center', gap: 10 }}>
-        <TextField label="할일을 입력하세요" size="small" variant="outlined" />
+        <TextField
+          label="할일을 입력하세요"
+          size="small"
+          variant="outlined"
+          onChange={handleChangeText}
+        />
         <Button variant="contained" size="large" color="primary">
           등록
         </Button>
       </div>
-      <ul></ul>
+      <ul>
+        {DB.map((item) => (
+          <li key={item.id} css={{ padding: '5px 0' }}>
+            {item.text}
+            <Button
+              variant="contained"
+              size="small"
+              color="secondary"
+              css={{ marginLeft: 10 }}
+            >
+              삭제
+            </Button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Home;
+export default Todo;
